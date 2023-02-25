c77d206b3c52e6dbb8bc6c042ea7b0544e7a25f6
Sorah Fukumori <her@sorah.jp>
2023-02-25 20:58:47 +0900

    bundle gem

---
+++ spec/himari_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Himari do
+  it "has a version number" do
+    expect(Himari::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
